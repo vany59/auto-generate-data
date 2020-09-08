c6d50bdc00f10a35d5a600b21fbaea72fcfb95a2
unknown <levanyy2@gmail.com>
2020-09-08 13:06:03 +0700

    upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# AUTO GENERATE FAKE DATA
+
+## Install
+
+```
+npm i
+```
+
+edit amount in ./index.js
+
+## Run
+
+```
+node index.js
+```
+
+the result will be copied to clipboard
+
+use [this](https://jsonformatter.curiousconcept.com/) for format data
---
+++ index.js
@@ -0,0 +1,39 @@
+var random_name = require("node-random-name");
+const _ = require("lodash");
+const uuid = require("uuid");
+const clipboardy = require("clipboardy");
+
+var data = [];
+const amount = 5;
+
+var i = 0;
+while (i < amount) {
+  const name = random_name();
+  if (!_.includes(data, name)) {
+    i++;
+    data.push(name);
+  }
+}
+
+data = data.map((d) => {
+  const value = d.split(" ");
+  const firstName = value[0];
+  const lastName = value[1];
+  const username = firstName.toLowerCase() + lastName.toLowerCase();
+  const email = `${username}@gmail.com`;
+  const phone = "03" + (Math.random() * (99999999 - 10000000) + 100000000);
+  const id = uuid.v4();
+
+  return {
+    firstName,
+    lastName,
+    username,
+    email,
+    phone,
+    id,
+  };
+});
+
+clipboardy.writeSync(JSON.stringify(data));
+console.log("copied to clipboard");
+// console.log(data);
